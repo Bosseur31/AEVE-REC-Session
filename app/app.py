451e9a158791eec6a05538e8a9065594da361bf4
“Bosseur31” <aymeric.baptiste@gmail.com>
2021-03-24 12:25:55 +0100

    Base

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ class RunRecSession(object):
     def on_put(self, req, resp, name):
         """Handles GET requests"""
         resp.status = falcon.HTTP_200  # This is the default status
-        resp.body = 'Demarrage video avec benevole'
+        resp.body = 'Demarrage video {} avec benevole', name
 
 
 # falcon.API instances are callable WSGI apps
@@ -15,4 +15,4 @@ app = falcon.API()
 run_rec = RunRecSession()
 
 # things will handle all requests to the '/things' URL path
-app.add_route('/things/{name}', run_rec)
+app.add_route('/run_rec/{name}', run_rec)
