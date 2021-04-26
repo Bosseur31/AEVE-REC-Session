a71cfd098bcd3f69ee4221db2066ef3a1e4bd075
aymeric <aymeric.baptiste@gmail.com>
2021-04-26 13:16:56 +0200

    Add status rec option and add BDD for info rec

--- app/app.py
+++ app/app.py
@@ -1,7 +1,5 @@
 import json
-
 import falcon
-
 import rec
 
 
@@ -15,10 +13,22 @@ class RunRecSession(object):
 
 class StopRecSession(object):
     def on_get(self, req, resp):
-        """Handles PUT requests"""
+        """Handles GET requests"""
         pid = rec.unrec_video()
         resp.status = falcon.HTTP_200  # This is the default status
         resp.body = json.dumps({"Pid": pid})
+
+class StatusRecSession(object):
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        status = rec.status_rec()
+        if status[0] == 'true':
+            info = rec.info_rec(status[1])
+            resp.status = falcon.HTTP_200
+            resp.body = json.dumps({"id": info[0], 'name': info[1], 'time': info[2], 'status': info[3]})
+        else:
+            resp.status = falcon.HTTP_200  # This is the default status
+            resp.body = json.dumps({"status": status})
 
 
 # falcon.API instances are callable WSGI apps
@@ -27,7 +37,9 @@ app = falcon.App()
 # Resources are represented by long-lived class instances
 run_rec = RunRecSession()
 stop_rec = StopRecSession()
+status_rec = StatusRecSession()
 
 # things will handle all requests to the '/things' URL path
 app.add_route('/run_rec/{name}', run_rec)
 app.add_route('/stop_rec', stop_rec)
+app.add_route('/status_rec', status_rec)
--- app/rec.py
+++ app/rec.py
@@ -4,42 +4,102 @@ import pickle
 import signal
 import subprocess
 from pathlib import Path
+import sqlite3
 
 
 def rec_video(name):
     # Déclaration de variable
+    ts = datetime.datetime.now().timestamp()
     timestamp = datetime.datetime.now()
     annee = timestamp.strftime('%Y')
     semaine = timestamp.strftime('%V le %m.%y')
     jour = timestamp.strftime('%d-%m-%y %Hh%M')
 
     # Création dossier
-    path = Path('/srv/aeve-rec-session/data/' + annee + '/Semaine-' + semaine + '/')
+    path = Path('/home/aymeric/Documents/data/' + annee + '/Semaine-' + semaine + '/')
     path.mkdir(parents=True, exist_ok=True)
 
     # Flux réseau caméra
     rstp_server = 'rtsp://admin@192.168.31.56/0/av0'
 
     # Nom fichier
-    file_directory = '/srv/aeve-rec-session/data/' + annee + '/Semaine-' + semaine + '/' + name + ' ' + jour + '.mp4'
+    file_directory = '/home/aymeric/Documents/data/' + annee + '/Semaine-' + semaine + '/' + name + ' ' + jour + '.mp4'
 
     # Démarrage VLC
     cmdbase = 'cvlc -I dummy ' + rstp_server + ' --sout="#transcode{vcodec=h264,acodec=mp3,vb=500,fps=30.0}:std{mux=mp4,dst=' + file_directory + ',access=file}"'
     process = subprocess.Popen(cmdbase, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
     pid = os.getpgid(process.pid)
-    f = open('/srv/aeve-rec-session/back/temp_var/out.ser', "wb")
+    f = open('/home/aymeric/Documents/data/out.ser', "wb")
     pickler = pickle.Pickler(f, pickle.HIGHEST_PROTOCOL)
     pickler.dump(pid)
     f.close()
+
+    # Mise en BDD
+    con = sqlite3.connect("/home/aymeric/Documents/data/bdd/rec_bdd.db")
+    cur = con.cursor()
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS rec(
+        id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+        name TEXT,
+        time INTEGER,
+        status INTEGER
+    )''')
+    donnees = (name, ts, 1)
+    cur.execute("INSERT INTO rec (name, time, status) VALUES (?, ?, ?)", donnees)
+    con.commit()
+    con.close()
 
     return pid
 
 
 def unrec_video():
-    f = open('/srv/aeve-rec-session/back/temp_var/out.ser', "rb")
+    f = open('/home/aymeric/Documents/data/out.ser', "rb")
     unpickler = pickle.Unpickler(f)
     pid = unpickler.load()
     f.close()
     os.killpg(pid, signal.SIGTERM)
 
+    con = sqlite3.connect("/home/aymeric/Documents/data/bdd/rec_bdd.db")
+    cur = con.cursor()
+    cur.execute("UPDATE rec SET status = 0")
+    con.commit()
+    con.close()
+
     return pid
+
+
+def status_rec():
+    con = sqlite3.connect("/home/aymeric/Documents/data/bdd/rec_bdd.db")
+    cur = con.cursor()
+    cur.execute("SELECT id FROM rec WHERE status = 1 ORDER BY id DESC LIMIT 1")
+    data = cur.fetchall()
+
+    # true = enregistrement en cours / false = pas d'enregistrement en cours
+    if len(data) == 0:
+        rec = 'false'
+    else:
+        rec = 'true'
+        rec_id = (','.join(map(str, next(zip(*data)))))
+
+    try:
+        return rec, rec_id
+    except NameError:
+        return rec
+
+
+def info_rec(rec_id):
+    con = sqlite3.connect("/home/aymeric/Documents/data/bdd/rec_bdd.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM rec WHERE id = ?", (rec_id,))
+    res = cur.fetchall()
+    for row in res:
+        rec_id = row[0]
+        rec_name = row[1]
+        rec_time = row[2]
+        if row[3] == 1:
+            rec_status = 'true'
+        else:
+            rec_status = 'false'
+    con.close()
+
+    return rec_id, rec_name, rec_time, rec_status
